1d1bee74f34b5ee95dbd1b3615ce7305ca41c5c8
prubioamzubiri <prubioam@zubiirmanteo.com>
2025-06-03 13:12:06 +0200

    a

--- 05-dockercompose/nextcloud/apps/activity/js/activity-api.mjs
+++
@@ -1,2 +0,0 @@
-import{r as t}from"./api-CKODZZjL.chunk.mjs";import"./logger-ByCR-oAD.chunk.mjs";import"./_commonjsHelpers-D9DevfhH.chunk.mjs";t();
-//# sourceMappingURL=activity-api.mjs.map
---
+++ 05-dockercompose/nextcloud/apps/viewer/l10n/uz.js
@@ -0,0 +1,21 @@
+OC.L10N.register(
+    "viewer",
+    {
+    "Unsaved changes" : "Saqlanmagan o'zgarishlar",
+    "Edit" : "Tahrirlash",
+    "Download" : "Download",
+    "Delete" : "Delete",
+    "Name" : "Name",
+    "Save" : "Save",
+    "Back" : "Orqaga",
+    "Reset" : "Qayta tiklash",
+    "Cancel" : "Cancel",
+    "Confirm" : "Confirm",
+    "Undo" : "Bekor qilish",
+    "Custom" : "Boshqa",
+    "Value" : "Qiymati",
+    "Line" : "Chiziq",
+    "Text" : "Matn",
+    "Size" : "Size"
+},
+"nplurals=1; plural=0;");
