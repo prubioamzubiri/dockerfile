20cc9e103edda79bcf73040d1f80f85e42f3e45f
prubioamzubiri <prubioam@zubiirmanteo.com>
2025-06-03 12:30:39 +0200

    awsome compose 2 igo

---
+++ 05-dockercompose/nextcloud/apps/firstrunwizard/l10n/lv.js
@@ -0,0 +1,27 @@
+OC.L10N.register(
+    "firstrunwizard",
+    {
+    "First run wizard" : "Pirmās palaišanas vednis",
+    "Schedule work & meetings, synced with all your devices." : "Ieplāno darbu un sapulces, kas sinhronizētas ar visām jūsu ierīcēm.",
+    "Keep your colleagues and friends in one place without leaking their private info." : "Turiet savus kolēģus un draugus vienuviet, neizpludinot viņu privāto informāciju.",
+    "Simple email app nicely integrated with Files, Contacts and Calendar." : "Vienkārša e-pasta lietotne labi apvienota ar Datnēm, Kontaktiem un Kalendāru.",
+    "App recommendation: Tasks" : "Lietotnes ieteikums: Uzdevumi",
+    "App recommendation: Forms" : "Lietotnes ieteikums: Veidlapas",
+    "Access files via WebDAV" : "Piekļūt datnēm caur WebDAV",
+    "Desktop client" : "Darbvirsmas klients",
+    "iOS app" : "iOS lietotne",
+    "About & What's new" : "Par un kas jauns",
+    "Close" : "Aizvērt",
+    "More about Nextcloud" : "Vairāk par Nextcloud",
+    "Explore more apps ↗" : "Izpētīt vairāk lietotņu ↗",
+    "Privacy" : "Privātums",
+    "Host your data and files where you decide." : "Izvēlies, kur mitināt savus datus un datnes!",
+    "Import and export anything you want with open standards." : "Ievieto un izgūsti jebko, ko vēlies, ar atvērtiem standartiem!",
+    "Community" : "Kopiena",
+    "Get the apps to sync your files" : "Iegūsti lietotnes savu datņu sinhronizēšanai",
+    "{productName} gives you access to your files wherever you are. Our easy to use desktop and mobile clients are available for all major platforms at zero cost." : "{productName} sniedz piekļuvi Tavām datnēm, lai kur arī Tu būtu. Mūsu viegli izmantojamie darbvirsmas un viedierīču klienti ir bez maksas pieejami visās galvenajās platformās.",
+    "Server address" : "Servera adrese",
+    "Read more" : "Lasīt vairāk",
+    "Forms: automatically sync with a spreadsheet" : "Veidlapas: automātiski sinhronizēt ar izklājlapu"
+},
+"nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n != 0 ? 1 : 2);");
---
+++ 05-dockercompose/nextcloud/apps/related_resources/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'nextcloud/related_resources',
+        'pretty_version' => 'dev-stable31',
+        'version' => 'dev-stable31',
+        'reference' => 'e30071d9cd066f6325a9ca62fa45b22b55d5afb9',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'nextcloud/related_resources' => array(
+            'pretty_version' => 'dev-stable31',
+            'version' => 'dev-stable31',
+            'reference' => 'e30071d9cd066f6325a9ca62fa45b22b55d5afb9',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
