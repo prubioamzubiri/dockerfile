91d71448fcac20b15456363d34a56d7d3c461d27
prubioamzubiri <prubioam@zubiirmanteo.com>
2025-06-03 13:13:18 +0200

    delete nextcloud

--- 05-dockercompose/nextcloud/apps/text/js/init-CLzSasj9.chunk.mjs
+++
@@ -1,2 +0,0 @@
-function e(t,a){switch(arguments.length){case 0:break;case 1:this.range(t);break;default:this.range(a).domain(t);break}return this}export{e as i};
-//# sourceMappingURL=init-CLzSasj9.chunk.mjs.map
--- 05-dockercompose/nextcloud/apps/text/js/text-public.mjs
+++
@@ -1,2 +0,0 @@
-import{l as e}from"./index-DfiDB3im.chunk.mjs";OCA.Text={RichWorkspaceEnabled:e("text","workspace_available"),OpenReadOnlyEnabled:e("text","open_read_only_enabled")};
-//# sourceMappingURL=text-public.mjs.map
