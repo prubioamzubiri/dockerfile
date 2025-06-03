55454e862f5f6e9ec7dd0b2b4c1abea101a66e0a
prubioamzubiri <prubioam@zubiirmanteo.com>
2025-06-03 12:30:00 +0200

    awsome compose igo

---
+++ 05-dockercompose/nextcloud/apps/bruteforcesettings/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'nextcloud/bruteforcesttings',
+        'pretty_version' => 'dev-stable31',
+        'version' => 'dev-stable31',
+        'reference' => 'cd251c0b2cf1f25dac453688f8b3346ee0ea354b',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'nextcloud/bruteforcesttings' => array(
+            'pretty_version' => 'dev-stable31',
+            'version' => 'dev-stable31',
+            'reference' => 'cd251c0b2cf1f25dac453688f8b3346ee0ea354b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
---
+++ 05-dockercompose/nextcloud/apps/circles/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'nextcloud/circles',
+        'pretty_version' => 'dev-stable31',
+        'version' => 'dev-stable31',
+        'reference' => 'fa13d5355b09977d8c18e8ae5e9f5885953a2241',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'nextcloud/circles' => array(
+            'pretty_version' => 'dev-stable31',
+            'version' => 'dev-stable31',
+            'reference' => 'fa13d5355b09977d8c18e8ae5e9f5885953a2241',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
